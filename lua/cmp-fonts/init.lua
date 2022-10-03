7d85e97c1da6ae87fb019fb47995803a2f792038
amarakon <amar.al-zubaidi45@tutanota.com>
2022-10-03 00:00:29 -0400

    `cmp-fonts/init.lua`: simplify code

--- lua/cmp-fonts/init.lua
+++ lua/cmp-fonts/init.lua
@@ -1,16 +1,15 @@
 local source = {}
 
-local items = require "cmp-fonts/items"
+function source.new()
+	return setmetatable({}, { __index = source })
+end
 
-function source.new() return setmetatable({}, { __index = source }) end
-function source.get_keyword_pattern() return "[A-Z][^[:blank:]]*" end
+function source.get_keyword_pattern()
+	return "[A-Z][^[:blank:]]*"
+end
 
-source.complete = function(self, request, callback)
-	if not vim.regex(self.get_keyword_pattern() .. "$"):match_str(request.context.cursor_before_line) then
-		return callback()
-	end
-	if not self.items then self.items = items end
-	callback(self.items)
+function source.complete(_, _, callback)
+	callback(require "cmp-fonts/items")
 end
 
 return source
