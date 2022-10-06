84c7e339a46a22c61212664808295f77e6a9542e
amarakon <amar.al-zubaidi45@tutanota.com>
2022-10-06 14:23:55 -0400

    Simplify the generation of the font list

--- lua/cmp-fonts/items.lua
+++ lua/cmp-fonts/items.lua
@@ -1,7 +1,5 @@
-local cmd = io.popen([[
-	fc-list --format="%{family}\n" | cut -d , -f 1 | sort | uniq |
-		sed -z "$ s/\n$//"
-]])
+local cmd = io.popen(
+	[[fc-list --format="%{family}\n" | cut -d , -f 1 | sort | uniq]])
 
 if cmd == nil then
 	return
@@ -14,7 +12,6 @@ for i,v in function() return fontstr:find("\n", pos, true) end do
 	table.insert(fonttbl, { label = fontstr:sub(pos, i - 1) })
 	pos = v + 1
 end
-table.insert(fonttbl, { label = string.sub(fontstr, pos) })
 
 cmd:close()
 
